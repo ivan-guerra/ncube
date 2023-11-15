2ba244edf441d1611c5a876ad6cd8c340ed66688
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-11-14 22:23:08 -0800

    Introduce the first supported shape, a cube.

---
+++ include/shapes/cube.h
@@ -0,0 +1,14 @@
+#ifndef CUBE_H_
+#define CUBE_H_
+
+#include "common/types.h"
+
+namespace nshapes {
+namespace shapes {
+
+extern const Faces3D kCube;
+
+}  // namespace shapes
+}  // namespace nshapes
+
+#endif
